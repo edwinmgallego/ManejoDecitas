9e7dbf317d36b5091a6633d8c50401f442551492
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-05-03 11:55:54 -0500

    mi primer  commit  que nervios, creando y recordando mi primera clase en  java

---
+++ Programaciionorientadoaobjetosenjava/src/com/company/Doctor.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Doctor {
+int id;
+String name;
+String speciality;
+//comportamientos
+
+    public void showName(){
+        System.out.println(name);
+        System.out.println(speciality);
+
+    }
+}
--- Programaciionorientadoaobjetosenjava/src/com/company/Main.java
+++ Programaciionorientadoaobjetosenjava/src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Doctor myDoctor;
+        myDoctor = new Doctor();
+        myDoctor.name = "edwin gallego";
+        myDoctor.speciality= "medico del  amor.";
+        myDoctor.showName();
         System.out.println("hola  tqm");
     }
 }
